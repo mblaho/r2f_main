d010940072053672bf15496b0457121f10584b34
Michal Blaho <blaho@humusoft.sk>
2026-01-30 15:41:53 +0100

    small change

--- testArduino.m
+++ testArduino.m
@@ -1,14 +1,12 @@
-%[text] sdfsdfsdfdsf
+%[text] Nadpis
 % vytvorenie objektu a pre spojenie s Arduinom
 a=arduino
-
 %%
-
 clear
 % clear a
 
 %[appendix]{"version":"1.0"}
 %---
 %[metadata:view]
-%   data: {"layout":"onright"}
+%   data: {"layout":"inline"}
 %---
