9ef0aa265fb9c2663c75bb6464522c29a3119f95
Michal Blaho <blaho@humusoft.sk>
2026-01-30 15:50:19 +0100

    2 text

--- testArduino.m
+++ testArduino.m
@@ -1,16 +1,14 @@
-%[text] Nadpis
+%% 
+% Nadpis
+
 % vytvorenie objektu a pre spojenie s Arduinom
 a=arduino('COM3','MEGA2560',"Libraries","Ultrasonic")
-%%
-%[text] dsfsdf
+%% 
+% dsfsdf
+
 ultra = ultrasonic(a,'D2','D3')
-%%
-%[text] sdfsdf
+%% 
+% sdfsdf
+
 clear
 % clear a
-
-%[appendix]{"version":"1.0"}
-%---
-%[metadata:view]
-%   data: {"layout":"inline"}
-%---
