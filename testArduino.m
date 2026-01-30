52da071b8af3e5ed4a430aee08e939582b08aa39
Michal Blaho <blaho@humusoft.sk>
2026-01-30 15:45:23 +0100

    add LED

--- testArduino.m
+++ testArduino.m
@@ -2,6 +2,11 @@
 % vytvorenie objektu a pre spojenie s Arduinom
 a=arduino
 %%
+% Zapnutie LED na pine D7
+a.writeDigitalPin('D7',1)
+% Vypnutie LED na pine D7
+% a.writeDigitalPin('D7',0)
+%%
 clear
 % clear a
 
